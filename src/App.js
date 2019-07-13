8830cc19d9e0130c48ad66b5a0097e683285163a
Som Ramnani <som.ramnani@gmail.com>
2019-07-13 02:31:35 -0500

    added react app files, web3, and instances for the contract in the app.js

---
+++ src/App.js
@@ -0,0 +1,76 @@
+    
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery'
+
+
+class App extends Component {
+  state = {
+   manager: '',
+   players: [],
+   balance: '',
+   value: ''
+ };
+ 
+ async componentDidMount() {
+  const manager = await lottery.methods.manager().call();
+  const players = await lottery.methods.getPlayers().call();
+  const balance = await web3.eth.getBalance(lottery.options.address);
+
+  this.setState({manager, players, balance});
+};
+
+onSubmit = async (event) => {
+  event.preventDefault();
+  const accounts = await web3.eth.getAccounts();
+  await lottery.methods.enter().send({
+    from: accounts[0],
+    value: web3.utils.toWei(this.state.value, 'ether')
+  });
+};
+
+render(){
+  return (
+    <div>
+      <h2>Lottery Contract</h2>
+
+      <p>
+        This contract is managed by {this.state.manager}
+      </p>
+
+      <p>
+        There are currently {this.state.players.length} people entered,
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+      </p>
+
+      <p>
+        This contact address that is deployed on Rinksby is:  
+        <a target = "_blank" href="https://rinkeby.etherscan.io/address/0x698afdfff2ed5e71f9a9b59ea4b0fc6cb9f688a6">0x698afDffF2Ed5e71f9A9B59ea4b0fc6CB9f688a6</a> 
+      </p>
+
+
+
+      <hr />
+
+      <form onSubmit = {this.onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value = {this.state.value}
+            onChange = {event => this.setState({value: event.target.value})}
+            />
+        </div>
+        <button>Enter</button>
+      </form>
+
+    </div>
+    );
+  }
+}
+
+
+export default App;
